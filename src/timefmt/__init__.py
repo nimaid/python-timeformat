843b2e1af876876c5d960cf75a54e8c8f8d727ab
Ella Jameson <ellagjameson@gmail.com>
2024-01-01 04:26:26 -0700

    Update __init__.py

--- src/timefmt/__init__.py
+++ src/timefmt/__init__.py
@@ -1,6 +1,6 @@
 """Provides methods to manipulate and format various time-based objects into human-readable strings."""
 
-__version__ = "1.1.10"
+__version__ = "1.1.11"
 
 __all__ = [
     "dt", "td",
